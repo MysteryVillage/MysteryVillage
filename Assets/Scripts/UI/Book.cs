21886dab37d09d8b36a3898987131222bf773b5a
Sophia Wild <sosowalld353@gmail.com>
2023-04-29 15:52:35 +0200

    start implement book ui

---
+++ Assets/Scripts/UI/Book.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Book : MonoBehaviour
+{
+    [SerializeField] float _pageSpeed = 0.5f;
+    [SerializeField] List<Transform> _pages;
+    int _index = -1;
+
+    public void RotateNext()
+    {
+        _index++;
+        float angle = 180;
+        Rotate(angle, -1);
+    }
+
+    public void RotateBack()
+    {
+        float angle = 0;
+        Rotate(angle, 0);
+    }
+
+    private void Rotate(float angle, int direction)
+    {
+        StopAllCoroutines();
+        StartCoroutine(RotateOverTime(angle, direction));
+    }
+
+    IEnumerator RotateOverTime(float angle, int direction)
+    {
+        float timeElapsed = 0;
+        float angle1 = 1;
+        while (angle1 > 0.1f )
+        {
+            Quaternion targetRotation = Quaternion.Euler(0, angle, 0);
+            timeElapsed += Time.deltaTime;
+            _pages[_index].rotation = Quaternion.Lerp(_pages[_index].rotation, targetRotation, timeElapsed * _pageSpeed);
+            angle1 = Quaternion.Angle(_pages[_index].rotation, targetRotation);
+            print("angle1: " + angle1);
+            print(_pages[_index]);
+
+            yield return null;
+        }
+        if (direction == 0)
+        {
+            _index--;
+        }
+    }
+}
