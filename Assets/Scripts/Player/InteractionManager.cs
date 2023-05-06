caf06c915d69906c4f0e931a03eb43b422abdd5c
Philipp Ehrenberg <ehrenberg.ph@gmail.com>
2023-05-06 16:35:18 +0200

    Add interactable items

---
+++ Assets/Scripts/Player/InteractionManager.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InteractionManager : MonoBehaviour
+{
+    public float checkRate = .05f;
+    private float lastCheckTime;
+    public float maxCheckDistance;
+    public LayerMask layerMask;
+
+    private GameObject curInteractGameObject;
+    private IIinteractable curInteractable;
+    public TextMeshProUGUI promptText;
+    private Camera cam;
+
+    private void Start()
+    {
+        cam = Camera.main;
+    }
+
+    private void Update()
+    {
+        if (Time.time - lastCheckTime > checkRate)
+        {
+            lastCheckTime = Time.time;
+
+            Ray ray = cam.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0));
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, maxCheckDistance, layerMask))
+            {
+                if (hit.collider.gameObject != curInteractGameObject)
+                {
+                    curInteractGameObject = hit.collider.gameObject;
+                    curInteractable = hit.collider.GetComponent<IIinteractable>();
+                    SetPromptText();
+                }
+            }
+            else
+            {
+                curInteractGameObject = null;
+                curInteractable = null;
+                promptText.gameObject.SetActive(false);
+            }
+            Gizmos.DrawRay(ray.origin, ray.direction);
+        }
+    }
+
+    void SetPromptText()
+    {
+        promptText.gameObject.SetActive(true);
+        promptText.text = string.Format("<b>[E]</b> {0}", curInteractable.GetInteractPrompt());
+    }
+
+    public void OnInteractInput(InputAction.CallbackContext context)
+    {
+        if (context.phase == InputActionPhase.Started && curInteractable != null)
+        {
+            curInteractable.OnInteract();
+            curInteractGameObject = null;
+            curInteractable = null;
+            promptText.gameObject.SetActive(false);
+        }
+    }
+}
+
+public interface IIinteractable
+{
+    string GetInteractPrompt();
+    void OnInteract();
+}
