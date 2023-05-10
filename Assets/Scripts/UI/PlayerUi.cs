5bd50df8653f59ad2eab98774842bfa1f6847473
Sophia Wild <sosowalld353@gmail.com>
2023-05-10 22:06:48 +0200

    add simple map controlling

--- Assets/Scripts/UI/PlayerUi.cs
+++ Assets/Scripts/UI/PlayerUi.cs
@@ -10,6 +10,7 @@ public class PlayerUi : MonoBehaviour
     [Header("Menue")] 
     private Transform playUi;
     private Transform book;
+    private Transform map;
     private bool menueUi;
 
     // Start is called before the first frame update
@@ -17,15 +18,27 @@ public class PlayerUi : MonoBehaviour
     {
         playUi = transform.GetChild(0);
         book = transform.GetChild(1);
+        map = transform.GetChild(2);
         menueUi = false;
     }
 
-    public void SwitchUI (InputAction.CallbackContext context)
+    public void SwitchToBook (InputAction.CallbackContext context)
     {
         if (context.phase == InputActionPhase.Started)
         {
             playUi.gameObject.SetActive(menueUi ? true : false);
             book.gameObject.SetActive(menueUi ? false : true);
+            menueUi = menueUi ? false : true;
+            menueUi = menueUi ? true : false;
+        }
+    }
+
+    public void SwitchToMap (InputAction.CallbackContext context)
+    {
+        if (context.phase == InputActionPhase.Started)
+        {
+            playUi.gameObject.SetActive(menueUi ? true : false);
+            map.gameObject.SetActive(menueUi ? false : true);
             menueUi = menueUi ? false : true;
             menueUi = menueUi ? true : false;
         }
