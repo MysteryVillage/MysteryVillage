f1a0e968a24bf29c217760c0fa96135b85c10adb
Philipp Ehrenberg <ehrenberg.ph@gmail.com>
2023-06-04 16:15:15 +0200

    Add debug bar

---
+++ Assets/Scripts/UI/DebugBar.cs
@@ -0,0 +1,80 @@
+using System;
+using Mirror;
+using TMPro;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using NetworkManager = Network.NetworkManager;
+
+namespace UI
+{
+    public class DebugBar : MonoBehaviour
+    {
+        public GameObject player;
+
+        [Header("FPS")] public TextMeshProUGUI fpsText;
+        private int _fps;
+        private float _fpsTimeCounter;
+        private float _lastFps;
+        public float fpsRefreshTime;
+
+        [Header("Input device")] public TextMeshProUGUI inputDevice;
+
+        [Header("Connection")] public TextMeshProUGUI connectionType;
+        public TextMeshProUGUI connectionIp;
+        public TextMeshProUGUI connectionTime;
+        public DateTime connectionTimestamp;
+
+        private void Start()
+        {
+            connectionTimestamp = DateTime.Now;
+            Debug.Log(connectionTimestamp);
+            Debug.Log(DateTime.Now);
+        }
+
+        private void Update()
+        {
+            // FPS calculation
+            if (_fpsTimeCounter < fpsRefreshTime)
+            {
+                _fpsTimeCounter += Time.deltaTime;
+                _fps++;
+            }
+            else
+            {
+                //This code will break if you set your m_refreshTime to 0, which makes no sense.
+                _lastFps = _fps / _fpsTimeCounter;
+                _fps = 0;
+                _fpsTimeCounter = 0.0f;
+            }
+
+            fpsText.text = "FPS: " + _lastFps;
+
+            // Input device
+            inputDevice.text = "Input Device: " + player.GetComponent<PlayerInput>().currentControlScheme;
+
+            // Connection
+            string connectionTypeData;
+            string connectionIpData;
+            if (NetworkServer.active)
+            {
+                connectionTypeData = "Host";
+                connectionIpData = "Host";
+            }
+            else
+            {
+                connectionTypeData = "Client";
+                connectionIpData = Mirror.NetworkManager.singleton.networkAddress;
+            }
+
+            connectionType.text = "Connectiontype: " + connectionTypeData;
+            connectionIp.text = "Connection IP: " + connectionIpData;
+            var connectionTimeSpan = DateTime.Now - connectionTimestamp;
+            connectionTime.text = "Connection Time: " + FormatTimespan(connectionTimeSpan);
+        }
+
+        string FormatTimespan(TimeSpan timeSpan)
+        {
+            return timeSpan.ToString(@"hh\:mm\:ss");
+        }
+    }
+}
--- Assets/Scripts/UI/PlayerUi.cs
+++ Assets/Scripts/UI/PlayerUi.cs
@@ -1,202 +1,190 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Reflection;
 using Inventory;
 using Mirror;
 using Player;
 using TMPro;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.Serialization;
-using UnityEngine.UIElements;
 using Yarn.Unity;
-using static UnityEngine.Rendering.DebugUI;
 using NetworkManager = Network.NetworkManager;
 
-public class PlayerUi : MonoBehaviour
+namespace UI
 {
-    [Header("Menu")] 
-    public Transform playUi;
-    private PlayerController _playerController;
-    public List<Transform> menuList;
-    private int _menuIndex = 0;
-    private bool _menuOpen = false;
-    public GameObject pauseMenu;
-
-    [Header("Dialogue")]
-    private DialogueRunner _dialogue;
-    public GameObject dialogueText;
-    public GameObject dialogueOptions;
-
-    [Header("Debug")] 
-    public TextMeshProUGUI inputDevice;
-
-    // Start is called before the first frame update
-    void Start()
+    public class PlayerUi : MonoBehaviour
     {
-        _playerController = transform.parent.GetComponent<PlayerController>();
-        _dialogue = GameObject.Find("Dialogue").GetComponent<DialogueRunner>();
-        _dialogue.dialogueViews[0] = dialogueText.GetComponent<LineView>();
-        _dialogue.dialogueViews[1] = dialogueOptions.GetComponent<OptionsListView>();
-    }
+        [Header("Menu")] 
+        public Transform playUi;
+        private PlayerController _playerController;
+        public List<Transform> menuList;
+        private int _menuIndex = 0;
+        private bool _menuOpen = false;
+        public GameObject pauseMenu;
 
-    private void Update()
-    {
-        inputDevice.text = transform.parent.GetComponent<PlayerInput>().currentControlScheme;
-    }
+        [Header("Dialogue")]
+        private DialogueRunner _dialogue;
+        public GameObject dialogueText;
+        public GameObject dialogueOptions;
 
-    public void SwitchTo(int index)
-    {
-        HideAll();
-        menuList[index].gameObject.SetActive(true);
-    }
-
-    public void HideAll()
-    {
-        for (int i = 0; i < menuList.Count; i++)
+        // Start is called before the first frame update
+        void Start()
         {
-            menuList[i].gameObject.SetActive(false);
+            _playerController = transform.parent.GetComponent<PlayerController>();
+            _dialogue = GameObject.Find("Dialogue").GetComponent<DialogueRunner>();
+            _dialogue.dialogueViews[0] = dialogueText.GetComponent<LineView>();
+            _dialogue.dialogueViews[1] = dialogueOptions.GetComponent<OptionsListView>();
         }
-    }
 
-    public void OpenBookButton(InputAction.CallbackContext context)
-    {
-        if (context.started)
+        public void SwitchTo(int index)
         {
-            _menuIndex = 0;
-            Open(_menuIndex);
+            HideAll();
+            menuList[index].gameObject.SetActive(true);
         }
-    }
 
-    public void OpenMapButton(InputAction.CallbackContext context)
-    {
-        if (context.started)
+        public void HideAll()
         {
-            _menuIndex = 1;
-            Open(_menuIndex);
-        }
-    }
-
-    public void OpenInventoryButton(InputAction.CallbackContext context)
-    {
-        if (context.started)
-        {
-            _menuIndex = 2;
-            Open(_menuIndex);
-            transform.parent.GetComponent<PlayerInventory>().ClearSelectedItemWindow();
-        }
-    }
-
-    public void Open(int index)
-    {
-        _menuOpen = true;
-        menuList[index].gameObject.SetActive(true);
-        playUi.gameObject.SetActive(false);
-        _playerController.SetActionMap("UI");
-    }
-
-    public void MenuSwitchRight(InputAction.CallbackContext context)
-    {
-        if (context.started)
-        {
-            if (_menuIndex < menuList.Count - 1)
+            for (int i = 0; i < menuList.Count; i++)
             {
-                _menuIndex++;
-                SwitchTo(_menuIndex);
+                menuList[i].gameObject.SetActive(false);
             }
         }
-    }
 
-    public void MenuSwitchLeft(InputAction.CallbackContext context)
-    {
-        if (context.phase == InputActionPhase.Started)
+        public void OpenBookButton(InputAction.CallbackContext context)
         {
-            if (_menuIndex > 0)
+            if (context.started)
             {
-                _menuIndex--;
-                SwitchTo(_menuIndex);
+                _menuIndex = 0;
+                Open(_menuIndex);
             }
         }
-    }
 
-    public void CloseUI(InputAction.CallbackContext context)
-    {
-        if (context.started)
+        public void OpenMapButton(InputAction.CallbackContext context)
         {
-            HideUIElements();
-        }
-    }
-
-    void HideUIElements()
-    {
-        _menuOpen = false;
-        playUi.gameObject.SetActive(true);
-        HideAll();
-        _playerController.SetActionMap("Player");
-    }
-
-    public void OnInteract(InputAction.CallbackContext context)
-    {
-        if (context.started && _menuIndex == 2)
-        {
-            transform.parent.GetComponent<PlayerInventory>().OnDropItemButton();
-        }
-    }
-
-    public void AdvanceText(InputAction.CallbackContext context)
-    {
-        if (context.started) dialogueText.GetComponent<LineView>().UserRequestedViewAdvancement();
-    }
-
-    public void PauseButton(InputAction.CallbackContext context)
-    {
-        if (context.started)
-        {
-            if (pauseMenu.activeInHierarchy)
+            if (context.started)
             {
-                ClosePauseMenu();
-            }
-            else
-            {
-                _playerController.SetActionMap("Pause");
-                pauseMenu.SetActive(true);
+                _menuIndex = 1;
+                Open(_menuIndex);
             }
         }
-    }
 
-    public void ClosePauseMenu()
-    {
-        _playerController.SetActionMap("Player");
-        pauseMenu.SetActive(false);
-    }
-
-    public void DisconnectButton()
-    {
-        var manager = GameObject.Find("NetworkManager").GetComponent<NetworkManager>();
-        if (NetworkServer.active)
+        public void OpenInventoryButton(InputAction.CallbackContext context)
         {
-            manager.StopHost();
+            if (context.started)
+            {
+                _menuIndex = 2;
+                Open(_menuIndex);
+                transform.parent.GetComponent<PlayerInventory>().ClearSelectedItemWindow();
+            }
         }
 
-        if (NetworkClient.active)
+        public void Open(int index)
         {
-            manager.StopClient();
+            _menuOpen = true;
+            menuList[index].gameObject.SetActive(true);
+            playUi.gameObject.SetActive(false);
+            _playerController.SetActionMap("UI");
         }
-    }
 
-    public void ResetButton()
-    {
-        var pos = GameObject.Find("NetworkManager").GetComponent<NetworkManager>().GetStartPosition();
-        transform.parent.GetComponent<NetworkTransformReliable>().RpcTeleport(pos.position);
-        ClosePauseMenu();
-    }
+        public void MenuSwitchRight(InputAction.CallbackContext context)
+        {
+            if (context.started)
+            {
+                if (_menuIndex < menuList.Count - 1)
+                {
+                    _menuIndex++;
+                    SwitchTo(_menuIndex);
+                }
+            }
+        }
 
-    public void InputChangeButton()
-    {
-        // ..
-        var input = transform.parent.GetComponent<PlayerInput>();
-        Debug.Log(input.currentControlScheme);
+        public void MenuSwitchLeft(InputAction.CallbackContext context)
+        {
+            if (context.phase == InputActionPhase.Started)
+            {
+                if (_menuIndex > 0)
+                {
+                    _menuIndex--;
+                    SwitchTo(_menuIndex);
+                }
+            }
+        }
+
+        public void CloseUI(InputAction.CallbackContext context)
+        {
+            if (context.started)
+            {
+                HideUIElements();
+            }
+        }
+
+        void HideUIElements()
+        {
+            _menuOpen = false;
+            playUi.gameObject.SetActive(true);
+            HideAll();
+            _playerController.SetActionMap("Player");
+        }
+
+        public void OnInteract(InputAction.CallbackContext context)
+        {
+            if (context.started && _menuIndex == 2)
+            {
+                transform.parent.GetComponent<PlayerInventory>().OnDropItemButton();
+            }
+        }
+
+        public void AdvanceText(InputAction.CallbackContext context)
+        {
+            if (context.started) dialogueText.GetComponent<LineView>().UserRequestedViewAdvancement();
+        }
+
+        public void PauseButton(InputAction.CallbackContext context)
+        {
+            if (context.started)
+            {
+                if (pauseMenu.activeInHierarchy)
+                {
+                    ClosePauseMenu();
+                }
+                else
+                {
+                    _playerController.SetActionMap("Pause");
+                    pauseMenu.SetActive(true);
+                }
+            }
+        }
+
+        public void ClosePauseMenu()
+        {
+            _playerController.SetActionMap("Player");
+            pauseMenu.SetActive(false);
+        }
+
+        public void DisconnectButton()
+        {
+            var manager = GameObject.Find("NetworkManager").GetComponent<NetworkManager>();
+            if (NetworkServer.active)
+            {
+                manager.StopHost();
+            }
+
+            if (NetworkClient.active)
+            {
+                manager.StopClient();
+            }
+        }
+
+        public void ResetButton()
+        {
+            var pos = GameObject.Find("NetworkManager").GetComponent<NetworkManager>().GetStartPosition();
+            transform.parent.GetComponent<NetworkTransformReliable>().RpcTeleport(pos.position);
+            ClosePauseMenu();
+        }
+
+        public void InputChangeButton()
+        {
+            // ..
+            var input = transform.parent.GetComponent<PlayerInput>();
+            Debug.Log(input.currentControlScheme);
+        }
     }
 }
