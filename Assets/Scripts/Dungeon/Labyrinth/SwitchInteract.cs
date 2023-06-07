53d4d7a5affcd2e907b208eb348704456c229554
Philipp Ehrenberg <ehrenberg.ph@gmail.com>
2023-06-07 14:06:56 +0200

    Schalter hinzugefügt

--- Assets/Scripts/Dungeon/Labyrinth/SwitchInteract.cs
+++ Assets/Scripts/Dungeon/Labyrinth/SwitchInteract.cs
@@ -6,31 +6,32 @@ using UnityEditor.Rendering;
 using UnityEngine;
 
 
-/* Script um die T�ren im Labyrinth zu �ffnen 
+/* Script um die T�ren im Labyrinth zu Öffnen 
  * 
- * bei Aktivierung des Schalters wird die T�r, die im Inspector festegelegt wird ge�ffnet 
- * Momentan wird die T�r f�r 10 Sekunden nach Untenbewegt ( Nicht sch�n ) 
+ * bei Aktivierung des Schalters wird die Tür, die im Inspector festegelegt wird ge�ffnet 
+ * Momentan wird die Tür für 10 Sekunden nach Untenbewegt ( Nicht sch�n ) 
  */
 
 public class SwitchInteract : NetworkBehaviour, IIinteractable
 {
-    [SerializeField] private GameObject door;
-    [SerializeField] private GameObject lever;
+    [SerializeField] private GameObject doorOpen1,doorClose,doorOpen2;
+    [SerializeField] private GameObject red_finish;
+    //[SerializeField] private GameObject lever;
     [SerializeField] private Animator levlerAnimation = null;
-    
+
 
 
 
     private bool opening = false;
     private float counter = 0;
     private float speed = 30f;
-  
+
 
     public string GetInteractPrompt()
     {
         return string.Format("Activate Switch {0}", "here");
     }
-    
+
     public void OnInteract(uint networkIdentifier)
     {
         opening = true;
@@ -43,26 +44,26 @@ public class SwitchInteract : NetworkBehaviour, IIinteractable
     {
         if (opening)
         {
-            
+
             OpenDoor();
         }
-      
+
     }
-    
+
     [ClientRpc]
     private void OpenDoor()
     {
-        if (door == null) return;
-        if(door.transform.localScale.y > 0)
+        if (doorOpen1 == null) return;
+        if (doorOpen1.transform.localScale.y > 0)
         {
-            door.transform.localScale += new Vector3(0, -(1 * Time.deltaTime * speed), 0);
-            door.transform.position += new Vector3(0, -(1 * Time.deltaTime * (speed/100)), 0); 
+            doorOpen1.transform.localScale += new Vector3(0, -(1 * Time.deltaTime * speed), 0);
+            doorOpen1.transform.position += new Vector3(0, -(1 * Time.deltaTime * (speed / 100)), 0);
         }
     }
     private void AnimateSwitch()
     {
-        if (lever == null && levlerAnimation == null) return;
-        levlerAnimation.Play("Switch_Animation", 0, 0.0f);
-        
+        if ( levlerAnimation == null) return;
+        levlerAnimation.Play("SwitchAnimation", 0, 0.0f);
+
     }
 }
