b22951382188eba697582763aa884599e4a5209f
Sophia Wild <sosowalld353@gmail.com>
2023-06-18 23:44:12 +0200

    Steuerkreuz für Tastatur ersetzen

--- Assets/Scripts/Inputs/SwitchableIcon.cs
+++ Assets/Scripts/Inputs/SwitchableIcon.cs
@@ -12,9 +12,17 @@ namespace Inputs
         
         private InputIconMap _iconMap;
 
+        public Sprite gamepadNavigation;
+        public Sprite keyboardNavigation;
+        private Image _navigation;
+        private SpriteRenderer _spriteRenderer;
+
+
         private void Start()
         {
             _iconMap = GetComponentInParent<PlayerUi>().iconMap;
+            _navigation = GameObject.Find("DPad").GetComponent<Image>();
+            //_spriteRenderer = GameObject.Find("DPad").GetComponent<SpriteRenderer>();
         }
 
         private void Update()
@@ -26,10 +34,12 @@ namespace Inputs
                     if (GetComponentInParent<PlayerInput>().currentControlScheme == "Gamepad")
                     {
                         GetComponent<Image>().sprite = _iconMap.map[i].gamepadIcon;
+                        _navigation.sprite = gamepadNavigation;
                     }
                     else
                     {
                         GetComponent<Image>().sprite = _iconMap.map[i].keyboardIcon;
+                        _navigation.sprite = keyboardNavigation;
                     }
                 }
             }
