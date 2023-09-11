3a41dde342d3ee4cc5d52263e59a61bbf12cd73a
Thorben Wolff <thorbenwolff123@gmail.com>
2023-09-11 20:46:05 +0200

    LT - Tür animation hinzugefügt

--- Assets/Scripts/Dungeon/Leuchtturm/PipelineManager.cs
+++ Assets/Scripts/Dungeon/Leuchtturm/PipelineManager.cs
@@ -1,14 +1,16 @@
+using Mirror;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PipelineManager : MonoBehaviour
+public class PipelineManager : NetworkBehaviour
 {
     [SerializeField] List<Transform> _tubes;
     [SerializeField] List<Renderer> _lamps;
+    [SerializeField] List<Animator> _doors;
     [SerializeField] Material _red;
     [SerializeField] Material _green;
-
+   
     private float[] rotation;
 
     private bool D;
@@ -45,6 +47,8 @@ public class PipelineManager : MonoBehaviour
             if (rotation[1] < 10)
             {
                 _lamps[8].material = _green;
+                // AnimateDoor(1,true); // Tür 1 wird geöffnet 
+                // AnimateDoor(1,false); // Tür 1 wird geschlossen
             }
             else
             {
@@ -88,5 +92,61 @@ public class PipelineManager : MonoBehaviour
             _lamps[0].material.color = white;
         }*/
 
+    }
+
+    [ClientRpc]
+    private void AnimateDoor( int door , bool open)
+    {
+        if (open)
+        {
+            switch (door)
+            {
+                case 1:
+                    if (_doors[0] != null) _doors[0].Play("LT_Tür_Animation_Open", 0, 0.0f);
+                    break;
+                case 2:
+                    if (_doors[1] != null) _doors[1].Play("LT_Tür_Animation_Open", 0, 0.0f);
+                    break;
+                case 3:
+                    if (_doors[2] != null) _doors[2].Play("LT_Tür_Animation_Open", 0, 0.0f);
+                    break;
+                case 4:
+                    if (_doors[3] != null) _doors[3].Play("LT_Tür_Animation_Open", 0, 0.0f);
+                    break;
+                case 5:
+                    if (_doors[4] != null) _doors[4].Play("LT_Tür_Animation_Open", 0, 0.0f);
+                    break;
+                case 6:
+                    if (_doors[5] != null) _doors[5].Play("LT_Tür_Animation_Open", 0, 0.0f);
+                    break;
+            }
+        }
+        else
+        {
+            
+            switch (door)
+            {
+                case 1:
+                    if (_doors[0] != null) _doors[0].Play("LT_Tür_Animation_Close", 0, 0.0f);
+                    break;
+                case 2:
+                    if (_doors[1] != null) _doors[1].Play("LT_Tür_Animation_Close", 0, 0.0f);
+                    break;
+                case 3:
+                    if (_doors[2] != null) _doors[2].Play("LT_Tür_Animation_Close", 0, 0.0f);
+                    break;
+                case 4:
+                    if (_doors[3] != null) _doors[3].Play("LT_Tür_Animation_Close", 0, 0.0f);
+                    break;
+                case 5:
+                    if (_doors[4] != null) _doors[4].Play("LT_Tür_Animation_Close", 0, 0.0f);
+                    break;
+                case 6:
+                    if (_doors[5] != null) _doors[5].Play("LT_Tür_Animation_Close", 0, 0.0f);
+                    break;
+            }
+
+        }
+        
     }
 }
