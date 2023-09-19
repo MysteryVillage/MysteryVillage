4e353205d1947eeecbc7e2710982abdf82f928ab
Sophia Wild <sosowalld353@gmail.com>
2023-09-19 20:53:07 +0200

    Add all Quests for LT

--- Assets/Scripts/Dungeon/Leuchtturm/checkLightsLT.cs
+++ Assets/Scripts/Dungeon/Leuchtturm/checkLightsLT.cs
@@ -1,18 +1,25 @@
+using Events;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.InputSystem.Controls.AxisControl;
 
 public class checkLightsLT : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] List<Renderer> _lamp;
+    [SerializeField] Material _red;
+    [SerializeField] Material _green;
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (_lamp[0].material && _lamp[1].material && _lamp[2].material == _green)
+        {
+            EventSystem eventSystem = UnityEngine.EventSystems.EventSystem.current as EventSystem;
+            if (eventSystem)
+            {
+                eventSystem.onQuestEvent.Invoke("LampsAreGreen");
+            }
+        }
     }
 }
