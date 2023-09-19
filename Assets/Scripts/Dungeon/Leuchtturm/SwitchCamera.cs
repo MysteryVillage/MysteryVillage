bd6ec07f603d08724e3cb0836813b8682c4e34dc
Thorben Wolff <thorbenwolff123@gmail.com>
2023-09-19 18:42:44 +0200

    LT Cam Update

--- Assets/Scripts/Dungeon/Leuchtturm/SwitchCamera.cs
+++ Assets/Scripts/Dungeon/Leuchtturm/SwitchCamera.cs
@@ -1,19 +1,42 @@
 using UnityEngine;
 using Cinemachine;
+using Unity.VisualScripting;
 
 public class SwitchCamera : MonoBehaviour
 {
-    public CinemachineVirtualCamera camera1;
-    public CinemachineVirtualCamera camera2;
-    
+    public CinemachineVirtualCamera cam;
 
+
+    private Vector3 defaultCam = new Vector3(1f, 0f, 0f);
+    private Vector3 LTcam = new Vector3(0.4f, 0.2f, -1f);
+    private CinemachineComposer composer;
+    private Cinemachine3rdPersonFollow follow;
     private bool isUsingCamera1 = true;
- 
-    private void OnTriggerEnter(Collider other)
+    
+   
+    private void Start()
+    {
+        //composer = cam.GetCinemachineComponent<CinemachineComposer>();
+        follow = cam.GetCinemachineComponent<Cinemachine3rdPersonFollow>();
+
+        
+        CamSwitch();
+    }
+    
+    private void OnTriggerStay(Collider other)
     {
         if (other.CompareTag("LT_CamTrigger"))
         {
-            Debug.Log("Trigger Cam Switch");
+            isUsingCamera1 = false;
+            CamSwitch();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("LT_CamTrigger"))
+        {
+            isUsingCamera1 = true;
             CamSwitch();
         }
     }
@@ -22,15 +45,19 @@ public class SwitchCamera : MonoBehaviour
     {
         if (isUsingCamera1)
         {
-            camera1.Priority = 10;
-            camera2.Priority = 20;
+            if (follow == null) return;
+            follow.CameraDistance = 4;
+            follow.ShoulderOffset = defaultCam;
+          
         }
         else
         {
-            camera1.Priority = 20;
-            camera2.Priority = 10;
+            if (follow == null) return;
+            follow.CameraDistance = 1;
+            follow.ShoulderOffset = LTcam;
         }
-     
+  
+
         isUsingCamera1 = !isUsingCamera1;
     }
 }
