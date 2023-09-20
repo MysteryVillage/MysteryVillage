4a4a2f4dc04028439cc11b4871dad91f4f0ae688
Philipp Ehrenberg <ehrenberg.ph@gmail.com>
2023-09-20 16:44:07 +0200

    Fix missing dialogue on client

--- Assets/Scripts/Dialogue/Intro.cs
+++ Assets/Scripts/Dialogue/Intro.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using Mirror;
 using Network;
 using UI;
@@ -18,6 +19,13 @@ namespace Dialogue
             // intro stuff
             
             FindObjectOfType<Crossfade>().FadeIn();
+
+            StartCoroutine(StartIntro());
+        }
+
+        private IEnumerator StartIntro()
+        {
+            yield return new WaitForSeconds(1);
             
             if (isServer) FindObjectOfType<DialogueManager>().StartActiveDialogue("Intro");
         }
