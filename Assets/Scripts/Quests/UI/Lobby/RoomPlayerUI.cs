072aa9a0f2fdab0299f78350167ee94ed1bf3aa8
Leonie T <leonie.trischler98@gmail.com>
2023-09-21 14:39:43 +0200

    fix

--- Assets/Scripts/Quests/UI/Lobby/RoomPlayerUI.cs
+++ Assets/Scripts/Quests/UI/Lobby/RoomPlayerUI.cs
@@ -77,7 +77,7 @@ namespace Quests.UI.Lobby
         {
             if (playerSlots[0].roomPlayer != null && playerSlots[1].roomPlayer != null)
             {
-                return (playerSlots[0].roomPlayer.character == "Colin" && playerSlots[1].roomPlayer.character == "Alina") || (playerSlots[1].roomPlayer.character == "Colin" && playerSlots[0].roomPlayer.character == "Alina");
+                return (playerSlots[0].roomPlayer.character == "Collin" && playerSlots[1].roomPlayer.character == "Alina") || (playerSlots[1].roomPlayer.character == "Collin" && playerSlots[0].roomPlayer.character == "Alina");
             }
 
             return true;
