2fc7dc9c0275103e0d66751d860371466ef0b423
Philipp Ehrenberg <ehrenberg.ph@gmail.com>
2023-09-21 18:05:45 +0200

    Remove campfire collider while it is hidden

--- Assets/Scripts/Environment/Hideable.cs
+++ Assets/Scripts/Environment/Hideable.cs
@@ -1,3 +1,4 @@
+using System;
 using Mirror;
 using UnityEngine;
 using Yarn.Unity;
@@ -6,9 +7,20 @@ namespace Environment
 {
     public class Hideable : NetworkBehaviour
     {
+        private Collider col;
+
+        private void Start()
+        {
+            col = GetComponent<Collider>();
+        }
+
         [YarnCommand("hide")]
         public void HideObject()
         {
+            if (col != null)
+            {
+                col.enabled = false;
+            }
             foreach (Transform child in transform)
             {
                 child.gameObject.SetActive(false);
@@ -18,6 +30,10 @@ namespace Environment
         [YarnCommand("show")]
         public void ShowObject()
         {
+            if (col != null)
+            {
+                col.enabled = true;
+            }
             foreach (Transform child in transform)
             {
                 child.gameObject.SetActive(true);
